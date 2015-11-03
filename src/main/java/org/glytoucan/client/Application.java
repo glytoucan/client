0f0ddbe068e5aa2f5681552e52680c18d2eac2ae
Nobuyuki Aoki <aokinobu@gmail.com>
2015-11-03 16:37:18 +0100

    cleanup

--- src/main/java/org/glytoucan/client/Application.java
+++ src/main/java/org/glytoucan/client/Application.java
@@ -2,9 +2,7 @@ package org.glytoucan.client;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
@@ -12,9 +10,6 @@ public class Application {
 	private static final Log logger = LogFactory.getLog(Application.class);
 	
 	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(
-				Application.class, args);
-		
 		AntiSpringJavaRest glyRest = new AntiSpringJavaRest();
 		String result = glyRest.register("RES\n" + "1b:b-dglc-HEX-1:5\n" + "2s:n-acetyl\n" + "3b:b-dgal-HEX-1:5\n" + "LIN\n"
 				+ "1:1d(2+1)2n\n" + "2:1o(4+1)3d");
