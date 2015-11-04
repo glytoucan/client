28c8de9a1f447e298384f9469f44b54185f52f3b
Nobuyuki Aoki <aokinobu@gmail.com>
2015-11-04 18:42:35 +0100

    fix test

--- src/test/java/org/glytoucan/client/RegistrationTests.java
+++ src/test/java/org/glytoucan/client/RegistrationTests.java
@@ -77,20 +77,20 @@ String sequence = "RES\n"
 		Assert.assertEquals(msg.getMessage(), "G00052MO");
 	}
 	
-	@Test(expected=HttpClientErrorException.class)
-//	@Test
+//	@Test(expected=HttpClientErrorException.class)
+	@Test
 	public void testRegistrationWithId() {
 String sequence = "RES\n"
 		+ "1b:b-dglc-HEX-1:5\n"
 		+ "2s:n-acetyl\n"
-		+ "3b:a-lgal-HEX-1:5|6:d\n"
-		+ "4b:b-dgal-HEX-1:5\n"
+		+ "3b:b-dgal-HEX-1:5\n"
+		+ "4b:a-lgal-HEX-1:5|6:d\n"
 		+ "5b:a-lgal-HEX-1:5|6:d\n"
 		+ "LIN\n"
 		+ "1:1d(2+1)2n\n"
 		+ "2:1o(3+1)3d\n"
-		+ "3:1o(4+1)4d\n"
-		+ "4:4o(2+1)5d";
+		+ "3:3o(2+1)4d\n"
+		+ "4:1o(4+1)5d\n";
 		/*RES
 	1b:b-dglc-HEX-1:5
 	2s:n-acetyl
@@ -104,12 +104,12 @@ String sequence = "RES\n"
 	4:4o(2+1)5d*/
 		Map<String, Object>  map = new HashMap<String, Object>();
 		map.put(GlycanSpec.SEQUENCE, sequence);
-		map.put(GlycanSpec.PUBLIC_DATABASE_STRUCTURE_ID, "2222");
+		map.put(GlycanSpec.PUBLIC_DATABASE_STRUCTURE_ID, "323");
 		
 		Map<String, Object>  results = glycanRest.registerStructure(map);
 		
 		logger.debug(results);
 		Message msg = (Message) results.get(GlycanSpec.MESSAGE);
-		Assert.assertEquals(msg.getMessage(), "G00052MO");
+		Assert.assertEquals(msg.getMessage(), "G00048MO");
 	}
 }
