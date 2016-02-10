eb08299836ad34b7f2628cb605dd2fdec2ab8922
aoki <aokinobu@gmail.com>
2016-02-10 15:23:00 +0900

    queryconfig

--- src/main/java/org/glytoucan/client/config/GlycanQueryConfig.java
+++ src/main/java/org/glytoucan/client/config/GlycanQueryConfig.java
@@ -1,46 +1,26 @@
-package org.glytoucan.client;
+package org.glytoucan.client.config;
 
 import java.util.HashMap;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.glytoucan.client.GlycanQueryRest;
+import org.glytoucan.model.spec.GlycanQuerySpec;
 import org.glytoucan.model.spec.GlycanSpec;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class GlycanConfig {
+public class GlycanQueryConfig {
 
-	private static final Log logger = LogFactory.getLog(GlycanConfig.class);
+	private static final Log logger = LogFactory.getLog(GlycanQueryConfig.class);
 
 	@Value("${api.hostname:http://test.api.glytoucan.org}")
 	private String hostname;
 
-	@Value("${api.glycan.context:/glycan}")
+	@Value("${api.glycan.context:/glycans}")
 	private String context;
-
-	@Value("${api.contributor.id}")
-	private String username;
-
-	@Value("${api.key}")
-	private String hash;
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getApiKey() {
-		return hash;
-	}
-
-	public void setApiKey(String hash) {
-		this.hash = hash;
-	}
 
 	public String getHostname() {
 		return hostname;
@@ -59,21 +39,22 @@ public class GlycanConfig {
 	}
 
 	@Bean
-	public GlycanSpec glycanSpec() {
+	public GlycanQuerySpec glycanQuerySpec() {
 		HashMap<String, Object> env = new HashMap<String, Object>();
 
 		env.put(GlycanSpec.HOSTNAME, hostname);
 		logger.debug("hostname is:>" + hostname + "<");
 		env.put(GlycanSpec.CONTEXT_PATH, context);
 		logger.debug("context is:>" + context + "<");
-		env.put(GlycanSpec.USERNAME, username);
-		logger.debug("username is:>" + username + "<");
-		env.put(GlycanSpec.API_KEY, getApiKey());
-		logger.debug("apikey is:>" + getApiKey() + "<");
+		// env.put(GlycanSpec.USERNAME, username);
+		// logger.debug("username is:>" + username + "<");
+		// env.put(GlycanSpec.API_KEY, getApiKey());
+		// logger.debug("apikey is:>" + getApiKey() + "<");
 
-		GlycanRest gr = new GlycanRest();
+		GlycanQueryRest gr = new GlycanQueryRest();
 		gr.setEnv(env);
 
 		return gr;
 	}
+
 }
