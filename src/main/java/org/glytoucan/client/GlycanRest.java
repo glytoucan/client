29175c20a4b690dcb1b1f4ff13247f5560d42b18
aoki <aokinobu@gmail.com>
2016-06-02 19:19:54 +0900

    image functionality

--- src/main/java/org/glytoucan/client/GlycanRest.java
+++ src/main/java/org/glytoucan/client/GlycanRest.java
@@ -1,11 +1,38 @@
 package org.glytoucan.client;
 
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import javax.imageio.ImageIO;
+import javax.xml.bind.DatatypeConverter;
+
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLContextBuilder;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
 import org.glytoucan.model.GlycanRequest;
 import org.glytoucan.model.Message;
 import org.glytoucan.model.spec.GlycanSpec;
@@ -16,6 +43,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.Base64Utils;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class GlycanRest implements GlycanSpec {
 
@@ -82,4 +111,135 @@ public class GlycanRest implements GlycanSpec {
 
 		return headers;
 	}
+
+  public String getImageWurcs(String hostname, String sequence) throws IOException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+    Map<String,Object> data = new HashMap<String,Object>();
+    data.put("structure", sequence);
+    data.put("encoding", "wurcs");
+    return getImage(hostname, sequence, data);
+  }
+  
+  public String getImage(String hostname, String sequence) throws IOException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+    Map<String,Object> data = new HashMap<String,Object>();
+    data.put("structure", sequence);
+    data.put("encoding", "glycoct");
+    return getImage(hostname, sequence, data);
+  }
+
+	 public String getImage(String hostname, String sequence, Map<String, Object> data) throws IOException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+//   String url = hostname + "/glyspace/service/glycans/image/glycan?format=png&notation=cfg&style=extended";
+   String url = hostname + "/glycans/image/glycan?format=png&notation=cfg&style=extended";
+ 
+   SSLContextBuilder builder = new SSLContextBuilder();
+     builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
+     SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
+             builder.build(), SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+     CloseableHttpClient client = HttpClients.custom().setSSLSocketFactory(
+             sslsf).build();
+   HttpPost post = new HttpPost(url);
+   logger.debug("url:>" + url);
+
+   // add header
+   post.setHeader("Content-Type", "application/json");
+
+   List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
+   
+   ObjectMapper mapper = new ObjectMapper();
+//   String output = mapper.writeValueAsString(data);
+   String output = "{\"format\":\"" + data.get("encoding") + "\",\"sequence\":\"" + sequence + "\"}"; 
+//   logger.debug("output:"+output);
+   logger.debug("output:"+output);
+   
+   post.setEntity(new StringEntity(output));
+   
+//   HttpClient client = HttpClientBuilder.create().build();
+
+   HttpResponse response = client.execute(post);
+   int code = response.getStatusLine().getStatusCode();
+   logger.debug("status code:"+code);
+   if (code != 200) {
+     return null;
+   }
+
+   BufferedReader rd = new BufferedReader(
+           new InputStreamReader(response.getEntity().getContent()));
+
+//   StringBuilder result = new StringBuilder();
+//   String line = "";
+//   while ((line = rd.readLine()) != null) {
+//     logger.debug("line:>" + line);
+//     result.append(line);
+//   }
+       BufferedImage bufimage = ImageIO.read(response.getEntity().getContent());
+
+   String image = encodeToString(bufimage, "png"); 
+   
+   return "data:image/png;base64," + image;
+ }
+ 
+ String getRegisteredImage(String hostname) throws IOException {
+   String url = hostname + "/glycans/image/glycan?format=png&notation=cfg&style=extended'";
+ 
+   HttpClient client = HttpClientBuilder.create().build();
+   HttpGet request = new HttpGet(url);
+   HttpResponse response = client.execute(request);
+   int code = response.getStatusLine().getStatusCode();
+   logger.debug("status code:"+code);
+   if (code != 200) {
+     return null;
+   }
+   BufferedReader rd = new BufferedReader(
+       new InputStreamReader(response.getEntity().getContent()));
+
+   StringBuilder result = new StringBuilder("data:image/png;base64,");
+   String line = "";
+   while ((line = rd.readLine()) != null) {
+     result.append(DatatypeConverter.printBase64Binary(line.getBytes()) + "\n");
+   }
+   return result.toString();
+ }
+ 
+   /**
+    * Decode string to image
+    * @param imageString The string to decode
+    * @return decoded image
+    */
+   public static BufferedImage decodeToImage(String imageString) {
+
+       BufferedImage image = null;
+       byte[] imageByte;
+       try {
+           imageByte = Base64.decodeBase64(imageString);
+           ByteArrayInputStream bis = new ByteArrayInputStream(imageByte);
+           image = ImageIO.read(bis);
+           bis.close();
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+       return image;
+   }
+
+   /**
+    * Encode image to string
+    * @param image The image to encode
+    * @param type jpeg, bmp, ...
+    * @return encoded string
+    */
+   public static String encodeToString(BufferedImage image, String type) {
+       String imageString = null;
+       ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+       try {
+           ImageIO.write(image, type, bos);
+           byte[] imageBytes = bos.toByteArray();
+
+           imageString = Base64.encodeBase64String(imageBytes);
+
+           bos.close();
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+       return imageString;
+   }
+   
 }
---
+++ src/test/java/org/glytoucan/client/WebServiceClientTest.java
@@ -0,0 +1,90 @@
+package org.glytoucan.client;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = {WebServiceClientTest.class})
+public class WebServiceClientTest {
+	private static final String hostname = "http://test.api.glytoucan.org";
+  public static Logger logger = (Logger) LoggerFactory
+			.getLogger(WebServiceClientTest.class);
+
+@Test
+	public void image() throws Exception {
+		GlycanRest gsClient = new GlycanRest();
+		String image = gsClient.getImage(hostname, "RES\\n"
+				+ "1b:x-dglc-HEX-x:x\\n"
+				+ "2b:b-dgal-HEX-1:5\\n"
+				+ "3b:a-dgal-HEX-1:5\\n"
+				+ "4b:b-dgal-HEX-1:5\\n"
+				+ "5s:n-acetyl\\n"
+				+ "LIN\\n"
+				+ "1:1o(4+1)2d\\n"
+				+ "2:2o(3+1)3d\\n"
+				+ "3:3o(3+1)4d\\n"
+				+ "4:4d(2+1)5n");
+		Assert.assertNotNull(image);
+		logger.debug(image);
+	}
+
+//	@Test
+	public void imageTest() throws Exception {
+	  GlycanRest gsClient = new GlycanRest();
+		String image = gsClient.getImage(hostname, "RES\\n"
+				+ "1b:x-dgal-HEX-1:5\\n"
+				+ "2b:x-dman-HEX-1:5\\n"
+				+ "3s:n-acetyl\\n"
+				+ "LIN\\n"
+				+ "1:1o(-1+1)2d\\n"
+				+ "2:1d(2+1)3n");
+		Assert.assertNotNull(image);
+		logger.debug(image);
+	}
+
+	@Test
+	public void imageTestmanglc() throws Exception {
+	  GlycanRest gsClient = new GlycanRest();
+		String image = gsClient.getImage(hostname, "RES\\n"
+				+ "1b:x-dman-HEX-1:5\\n"
+				+ "2b:x-dglc-HEX-1:5\\n"
+				+ "LIN\\n"
+				+ "1:1o(-1+1)2d\\n");
+		Assert.assertNotNull(image);
+		logger.debug(image);
+	}
+	
+	 @Test
+	  public void imageTestG00026MO() throws Exception {
+	    GlycanRest gsClient = new GlycanRest();
+	    String image = gsClient.getImageWurcs(hostname, "WURCS=2.0/4,5,4/[u2122h_2*NCC/3=O][a2122h-1b_1-5_2*NCC/3=O][a1122h-1b_1-5][a1122h-1a_1-5]/1-2-3-4-4/a4-b1_b4-c1_c3-d1_c6-e1");
+	    Assert.assertNotNull(image);
+	    logger.debug(image);
+	  }
+	
+//	@Test
+	public void testRead() throws IOException {
+	    /* Test image to string and string to image start */
+        BufferedImage img = ImageIO.read(new File("/home/aoki/workspace/rdf.glytoucan/src/test/resources/image.png"));
+        BufferedImage newImg;
+        String imgstr;
+        GlycanRest gsClient = new GlycanRest();
+        imgstr = gsClient.encodeToString(img, "png");
+        System.out.println(imgstr);
+        newImg = gsClient.decodeToImage(imgstr);
+        ImageIO.write(newImg, "png", new File("/tmp/CopyOfTestImage.png"));
+        /* Test image to string and string to image finish */
+	}
+}
