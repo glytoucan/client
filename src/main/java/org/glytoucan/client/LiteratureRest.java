8bee66db2e3e6e4b7fbf35e3be86bc0cb239d87c
aoki <aokinobu@gmail.com>
2016-11-21 15:19:21 +0900

    lit

---
+++ src/main/java/org/glytoucan/client/LiteratureRest.java
@@ -0,0 +1,79 @@
+package org.glytoucan.client;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.glytoucan.client.model.RegisterContributorResponse;
+import org.glytoucan.client.model.RegisterLiteratureRequestResponse;
+import org.glytoucan.model.GlycanRequest;
+import org.glytoucan.model.Message;
+import org.glytoucan.model.RegisterContributorRequest;
+import org.glytoucan.model.spec.GlycanClientRegisterSpec;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.Base64Utils;
+import org.springframework.web.client.RestTemplate;
+
+public class LiteratureRest extends AuthenticatedApi {
+
+  public static String ACCESSION_NUMBER="accession_number";
+  public static String PUBLICATION_ID="publication_id";
+  public static String REMOVE_FLAG="remove_flag";
+
+  private static final Log logger = LogFactory.getLog(LiteratureRest.class);
+
+  RestTemplate restTemplate = new RestTemplate();
+  
+  @Autowired
+  GlycanConfig glycanConfig;
+
+  private static String context = "/literature";
+  
+
+  public Map<String, Object> register(Map<String, Object> gmap) {
+    if (gmap.containsKey(USERNAME))
+      glycanConfig.setUsername((String) gmap.get(USERNAME));
+    if (gmap.containsKey(API_KEY))
+      glycanConfig.setApiKey((String) gmap.get(API_KEY));
+    
+    RegisterLiteratureRequestResponse reqres = new RegisterLiteratureRequestResponse();
+    reqres.setAccessionNumber((String) gmap.get(ACCESSION_NUMBER));
+    reqres.setPublicationId((String) gmap.get(PUBLICATION_ID));
+    reqres.setRemoveFlag((Boolean)gmap.get(REMOVE_FLAG));
+
+    String cmd = context + "/register";
+    @SuppressWarnings({ "unchecked", "rawtypes" }) 
+    HttpEntity<?> requestEntity = new HttpEntity(reqres, getHeaders());
+    final ResponseEntity<RegisterLiteratureRequestResponse> responseEntity = submit(requestEntity, cmd);
+    reqres = responseEntity.getBody();
+    gmap.put(MESSAGE, reqres.getResponseMessage());
+    return gmap;
+  }
+
+  private ResponseEntity<RegisterLiteratureRequestResponse> submit(HttpEntity<?> requestEntity, String cmd) {
+    restTemplate.setErrorHandler(new ErrorHandler());
+    logger.debug("request:>"+ glycanConfig.getHostname() + cmd);
+    return restTemplate.exchange(glycanConfig.getHostname() + cmd, HttpMethod.POST, requestEntity, RegisterLiteratureRequestResponse.class);
+  }
+
+  private HttpHeaders getHeaders() {
+    HttpHeaders headers = new HttpHeaders();
+
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+    String auth = glycanConfig.getUsername() + ":" + glycanConfig.getApiKey();
+    byte[] encodedAuthorisation = Base64Utils.encode(auth.getBytes());
+    headers.add(GlycanClientRegisterSpec.AUTHORIZATION_HEADER, GlycanClientRegisterSpec.AUTH_BASIC_HEADER + new String(encodedAuthorisation));
+
+    return headers;
+  }
+}
---
+++ src/test/java/org/glytoucan/client/LiteratureTest.java
@@ -0,0 +1,57 @@
+package org.glytoucan.client;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.glytoucan.client.config.ContributorConfig;
+import org.glytoucan.client.model.RegisterContributorResponse;
+import org.glytoucan.client.model.RegisterLiteratureRequestResponse;
+import org.glytoucan.client.model.ResponseMessage;
+import org.glytoucan.model.Message;
+import org.glytoucan.model.spec.GlycanClientRegisterSpec;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.HttpClientErrorException;
+
+@SpringApplicationConfiguration(classes = { Application.class, ContributorConfig.class })
+@RunWith(SpringJUnit4ClassRunner.class)
+@Configuration
+public class LiteratureTest {
+
+	private static final Log logger = LogFactory.getLog(LiteratureTest.class);
+
+	@Autowired
+	LiteratureRest rest;
+
+	// these work normally but not with dummy data in test properties.
+	@Test
+	public void testOnlyRegistration() {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put(LiteratureRest.USERNAME, "815e7cbca52763e5c3fbb5a4dccc176479a50e2367f920843c4c35dca112e33d");
+		map.put(LiteratureRest.API_KEY, "b83f8b8040a584579ab9bf784ef6275fe47b5694b1adeb82e076289bf17c2632");
+		map.put(LiteratureRest.ACCESSION_NUMBER, "G00029MO");
+		map.put(LiteratureRest.PUBLICATION_ID, "9565568");
+		Map<String, Object> results = rest.register(map);
+
+		ResponseMessage result = (ResponseMessage) results
+				.get(LiteratureRest.MESSAGE);
+		String accNum = (String) results.get(LiteratureRest.ACCESSION_NUMBER);
+		String id = (String) results.get(LiteratureRest.PUBLICATION_ID);
+
+		Assert.assertNotNull(result);
+		Assert.assertNotNull(id);
+		Assert.assertNotNull(accNum);
+		Assert.assertEquals("G00029MO", accNum);
+		Assert.assertEquals("9565568", id);
+
+		logger.debug(results);
+	}
+}
